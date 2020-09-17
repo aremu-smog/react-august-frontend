b647899a12131eef7314ac6c16c3770fa6a98193
aremu-smog <aremuoluwagbamila@gmail.com>
2020-09-17 01:38:38 +0000

    Feat: Mobile Menu

--- src/landing/components/Nav.js
+++ src/landing/components/Nav.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
 
+
 const Nav = () =>{
+    const [closed, setClosed] = useState(true)
+
+    const toggleMenu = () => {
+        setClosed(!closed)
+    }
+    
     return(
-        <div className="navigation">
+        <div className={closed ? "navigation":"navigation mobile-nav-open" }>
+            <div className="menu-icon" onClick={()=> toggleMenu()}>{closed ? "Menu":"Close"}</div>
             <div className="inner">
             <Link to="/"><div className="link">Home</div></Link>
             <Link to="/story"><div className="link">Story</div></Link>
