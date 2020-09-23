7eeb66cb8de3e4e75cb6c875853c27a51b39644e
aremu-smog <aremuoluwagbamila@gmail.com>
2020-09-23 01:25:09 +0000

    Fix: Add alt to arrow

--- src/landing/components/Resource.js
+++ src/landing/components/Resource.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Resource =({url, name})=>{
     return(
-        <div className="link resource" ><a href={url} target="_blank" rel="noopener noreferrer">{name}<img src="../landing/link.svg" /></a></div>
+        <div className="link resource" ><a href={url} target="_blank" rel="noopener noreferrer">{name}<img src="../landing/link.svg" alt="external link" /></a></div>
     )
 }
 
